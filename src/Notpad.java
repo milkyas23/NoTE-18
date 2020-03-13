ec9d92470bffd94cc1ce27853e11e9f7920c9313
MilkyasEyob.Habte <milkyas23>
2020-03-13 11:05:09 +0100

    copy markerad text

--- src/Notpad.java
+++ src/Notpad.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.channels.AcceptPendingException;
 
 public class Notpad {
     private JButton newOpenButton;
     private JPanel panel;
     private JTextArea textArea;
-    private JButton sparaButton1;
-    private JButton clearButton;
-    private JButton menuButton;
+    private JButton save;
+    private JButton copy;
+    private JButton menu;
+
+    public Notpad() {
+        copy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textArea.append( textArea.getSelectedText());
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Notpad");
